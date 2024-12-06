609cf4e22c662cc1d4c615ee3d9da48a38fdfed0
RIshi Thakkar <rishithakkar0555@gmail.com>
2024-12-06 21:34:31 +0530

    setting up and variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,12 @@
+
+console.log("hello world");
+const accountId = 2332234
+let accountName= "Rishi thakkar"
+let accountEmail= "rishithakkar0555@gmail.com"
+var accountBalace= 12333.233  // var is not reccoumanded .... because of the scope problem
+accountType=null;
+console.log(accountId);
+
+// way to print multimle code
+
+console.table([accountId,accountName,accountType,accountEmail,accountBalace]);
