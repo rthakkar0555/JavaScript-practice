db37826fa6302073c9914aa806f1b451dcf4f4b9
RIshi Thakkar <rishithakkar0555@gmail.com>
2024-12-07 12:39:50 +0530

    data types in js

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // using new version of js
+let name="rishi" // string
+let num=123 // number
+let bool=true // boolean
+let obj=null // object/ null is stand alone value
+// alert("any pop message for browser")
+console.log(typeof undefined)
+console.log(typeof null)
+console.log(typeof name)
+console.log(typeof NaN) // number
+console.log(typeof Infinity) // number
+
+// number
+// bigint => for big numbers
+// string
+// null => stand alone value but object
+// undefined => premitive type
+//symbol => unique
+
+// objects
